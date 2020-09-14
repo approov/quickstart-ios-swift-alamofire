3cc159437d055c7a87b664cad73c379f15486361
Richard Taylor <richardt@criticalblue.com>
2020-09-14 16:01:03 +0100

    Make header comments more consistent

--- shapes-app/ApproovShapes/ViewController.swift
+++ shapes-app/ApproovShapes/ViewController.swift
@@ -13,11 +13,6 @@
 // MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR
 // ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH
 // THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-/*****************************************************************************
- * Project:     Shapes Demo App
- * File:        ViewController.swift
- * Copyright(c) 2016 - 2020 by CriticalBlue Ltd.
- ****************************************************************************/
 
 import UIKit
 import Alamofire
